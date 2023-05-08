d16a9a8382a6bf602ed02047e353b7e90dcac1fc
woho <woho@woho.woho>
2023-05-08 23:31:53 +0200

    Adjust binary path in test-random-input.sh

--- test-random-input.sh
+++ test-random-input.sh
@@ -4,7 +4,7 @@ export LC_CTYPE=C
 for format in css js xml html; do
 	for i in {0..1000}; do
 		input=$(tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 1000)
-		minify $format <(echo -n -e "$input") > /dev/null
+		./build/native/minify $format <(echo -n -e "$input") > /dev/null
 		if [ $? -gt 1 ]; then
 			echo -n $input > input-causing-crash.txt
 			echo Crash to reproduce: minify $format input-causing-crash.txt
